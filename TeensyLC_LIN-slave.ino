523b64501bd5fcae398ad0720ffa74585854cabb
psitech <paul@siermann.nl>
2024-03-11 03:02:38 +0100

    Update TeensyLC_LIN-slave.ino, added comments

--- TeensyLC_LIN-slave.ino
+++ TeensyLC_LIN-slave.ino
@@ -3,63 +3,58 @@
 // https://developerhelp.microchip.com/xwiki/bin/view/applications/lin/data-link-layer/
 // https://linchecksumcalculator.machsystems.cz/
 
-elapsedMicros BreakLength;
+elapsedMicros BreakLength;  // define timer
 
-bool WaitForBreakStart = true;
-bool WaitForBreakEnd = false;
-bool ValidLinFrame = false;
+bool WaitForBreakStart = true;  // first state of ISR
+bool WaitForBreakEnd = false;   // second state of ISR
+bool ValidLinFrame = false;     // set if valid LIN break detected
 int linCS = 14;
 int RXpin = 9;
 uint8_t SYNC = 0x00;
 uint8_t PID = 0x00;
 uint8_t response[8] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 };
-uint8_t CRC = 0x49;  // with PID 0x50, LIN 2.x, enhanced checksum
+uint8_t CRC = 0x49;  // calculated with PID 0x50 [LIN 2.x, enhanced checksum]
 
 void setup() {
-  pinMode(linCS, OUTPUT);
-  digitalWrite(linCS, HIGH);  // enable MCP2004
-  Serial2.begin(19200);       // init hardware serial port #2, RX pin = 9
-
-  attachInterrupt(digitalPinToInterrupt(RXpin), RXisr, CHANGE); // attach to serial RX pin
+  pinMode(linCS, OUTPUT);                                        // CS pin of MCP2004
+  digitalWrite(linCS, HIGH);                                     // enable MCP2004
+  Serial2.begin(19200);                                          // init hardware serial port #2
+  attachInterrupt(digitalPinToInterrupt(RXpin), RXisr, CHANGE);  // attach to Serial2 RX pin 9
 }
 
 void loop() {
-  if (ValidLinFrame) {
-    ValidLinFrame = false;
-    
+  if (ValidLinFrame) {               // if valid LIN break detected
     while (!Serial2.available()) {}  // wait for serial data to become available
-    SYNC = Serial2.read();           // read 1 byte from serial
-
+    SYNC = Serial2.read();           // read SYNC byte from serial
     while (!Serial2.available()) {}  // wait for serial data to become available again
     PID = Serial2.read();            // read PID byte that will follow SYNC byte
-
-    if (PID == 0x50) {             // check whether we are addressed
-      Serial2.write(response, 8);  // if so, send 8 byte response
-      Serial2.write(CRC);          // and send CRC byte
-    }
-    WaitForBreakStart = true;
+    if (PID == 0x50) {               // check whether we are addressed
+      Serial2.write(response, 8);    // if so, send 8 byte response
+      Serial2.write(CRC);            // and send CRC byte
+    }                                //
+    ValidLinFrame = false;           // reset bool
+    WaitForBreakStart = true;        // set first state for RXisr
   }
 }
 
 void RXisr() {
-  if (WaitForBreakStart) {
-    if (digitalReadFast(RXpin) == LOW) {
-      BreakLength = 0;
-      WaitForBreakEnd = true;
-      WaitForBreakStart = false;
+  if (WaitForBreakStart) {                // first state
+    if (digitalReadFast(RXpin) == LOW) {  // if low
+      BreakLength = 0;                    // start timer
+      WaitForBreakStart = false;          // reset this state
+      WaitForBreakEnd = true;             // and go to second state
     }
   }
-
-  if (WaitForBreakEnd) {
-    if (digitalReadFast(RXpin) == HIGH) {
-      if ((BreakLength > 600) && (BreakLength < 800)) {
-        ValidLinFrame = true;
-        WaitForBreakEnd = false;
-      } else {
-        WaitForBreakStart = true;
-        WaitForBreakEnd = false;
-      }
-      Serial2.clear();
+  if (WaitForBreakEnd) {                                 // second state
+    if (digitalReadFast(RXpin) == HIGH) {                // if high, check break time
+      if ((BreakLength > 600) && (BreakLength < 800)) {  // if break time in valid range [usec]
+        WaitForBreakEnd = false;                         // reset this state
+        ValidLinFrame = true;                            // and set bool to be checked by loop()
+      } else {                                           // if break time not in valid range
+        WaitForBreakEnd = false;                         // reset this state
+        WaitForBreakStart = true;                        // and go to previous state
+      }                                                  //
+      Serial2.clear();                                   // clear serial buffer from any garbage
     }
   }
 }
